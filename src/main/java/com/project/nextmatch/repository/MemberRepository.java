79b2cb533785eb92b5c0acfc38f75fd5b6b20c45
ANMOOS <95952213+ANMOOS@users.noreply.github.com>
2025-10-03 02:22:02 +0900

    Add files via upload

--- src/main/java/com/project/nextmatch/repository/MemberRepository.java
+++ src/main/java/com/project/nextmatch/repository/MemberRepository.java
@@ -1,14 +1,9 @@
-package com.project.nextmatch.repository;
+package work.wish.wishlist1.repository;
 
-import com.project.nextmatch.domain.Member;
+import work.wish.wishlist1.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
+// Member 엔티티 (PK 타입은 Long)를 다루는 Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Optional<Member> findByUsername(String username);
-
+    // 필요한 경우, 나중에 findByUsername 등의 커스텀 메서드를 추가할 수 있습니다.
 }
